80528da803cba9467b225ea1447decc02761010d
Иван Васильев <52786880+0ceiros@users.noreply.github.com>
2020-10-07 22:52:16 +0300

    Пофиксил банер

--- src/components/GuestHeader.js
+++ src/components/GuestHeader.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default function GuestHeader() {
 
     return(
-        <div className="dark guest-header light-background">
-          Кафедра МОСИТ - выпускникам
+        <div className='outer-guest-header'>
+          <div className="dark guest-header light-background">
+            Кафедра МОСИТ - выпускникам
+          </div>
+          
         </div>
     );
     
