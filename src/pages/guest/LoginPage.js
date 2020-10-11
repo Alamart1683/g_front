c3c0c47520df6458a34a6e282cdc7eb23053c300
Иван Васильев <52786880+0ceiros@users.noreply.github.com>
2020-10-11 13:34:27 +0300

    Третья верстка логина, начал делать Popover

--- src/pages/guest/LoginPage.js
+++ src/pages/guest/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import { Form } from 'react-bootstrap';
+import { Form, OverlayTrigger, Popover } from 'react-bootstrap';
 import axios from 'axios';
 import { useAuthContext } from '../../auth/AuthContext';
 import { apiURL } from '../../Config';
@@ -12,11 +12,31 @@ export default function LoginPage() {
   const [password, setPassword] = useState("");
   const { setAuthTokens } = useAuthContext();
 
+  // Бутстрап отказывается менять вид Popover при использовании classname
+  // OverlayTrigger внедряет свой style и видимо переписывает его
+  const popoverContentStyle = {
+    font: 'PT Sans',
+    fontSize: '30px',
+    color: '#3A5985'
+  };
+
+  const errorPopover = (props) => (
+    <Popover {...props} id='errorPopover' >
+      <Popover.Title style={ popoverContentStyle }>
+        Ошибка!
+      </Popover.Title>
+      <Popover.Content id='errorPopoverContent' style={ popoverContentStyle }>
+        Неверный логин или пароль
+      </Popover.Content>
+    </Popover>
+  );
+
   // Обработка запроса на логин по введенным данным
   function postLogin() {
     const params = new URLSearchParams();
     params.append('email', email);
     params.append('password', password);
+    // TODO: Переписать через fetch
     axios({
       method: 'post',
       url: apiURL + '/authorization',
@@ -26,20 +46,20 @@ export default function LoginPage() {
       if (result.status === 200) {
         if (result.data.message === "Аккаунт не подтвержден" ||
             result.data.message === "Неверный логин или пароль") {
-          document.getElementById("errorMessage").style.visibility = 'visible';
-          document.getElementById("errorMessage").innerHTML = result.data.message;
+          //document.getElementById("errorMessage").style.visibility = 'visible';
+          //document.getElementById("errorMessage").innerHTML = result.data.message;
         }
         else {
           setAuthTokens(result.data);
           setLoggedIn(true);
         }
       } else {
-        document.getElementById("errorMessage").style.visibility = 'visible';
-        document.getElementById("errorMessage").innerHTML = "Ошибка подключения";
+        //document.getElementById("errorMessage").style.visibility = 'visible';
+        //document.getElementById("errorMessage").innerHTML = "Ошибка подключения";
       }
     }).catch(e => {
-      document.getElementById("errorMessage").style.visibility = 'visible';
-      document.getElementById("errorMessage").innerHTML = "Непредвиденная ошибка";
+      //document.getElementById("errorMessage").style.visibility = 'visible';
+      //document.getElementById("errorMessage").innerHTML = "Непредвиденная ошибка";
     });
   }
 
@@ -59,21 +79,23 @@ export default function LoginPage() {
               placeholder="Введите почту" className="size-24 loginForm-input"/>
           </Form.Group>
 
-          <p id = "errorMessage" className="size-21 red loginForm-label5">ErrorMessage</p>
-
           <Form.Group controlId="formLoginPassword" className='loginForm-formGroup'>
             <Form.Label className="size-21 dark loginForm-label4">Пароль</Form.Label>
             <Form.Control type="password" onChange={e => {setPassword(e.target.value);}} 
               placeholder="Введите пароль" className="size-24 loginForm-input"/>
             <Link to="/guest/forgotten_password">
                 <Form.Text className="size-21 dark loginForm-label2">
-                Забыли пароль? Восстановить
+                  Забыли пароль? Восстановить
                 </Form.Text>
             </Link>
           </Form.Group>
 
           <Form.Group controlId="formLoginSubmit" onClick={postLogin} className='loginForm-formGroup'>
-            <button type='submit' className="size-32 dark-background light loginForm-button">Войти в систему</button>
+            
+            <OverlayTrigger trigger='click' placement='right' overlay={errorPopover}>
+              <button type='submit' className="size-32 dark-background light loginForm-button">Войти в систему</button>
+            </OverlayTrigger>
+            
           </Form.Group>
 
           <p className="size-21 dark loginForm-label3">Или</p>
