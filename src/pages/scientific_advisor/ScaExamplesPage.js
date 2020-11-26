4f20a2c326261bc847fd625bf2daf620e28bf7a9
Иван Васильев <52786880+0ceiros@users.noreply.github.com>
2020-11-26 15:06:50 +0300

    Смена доступа для образцев и обновление создания образцов для науч рука

--- src/pages/scientific_advisor/ScaExamplesPage.js
+++ src/pages/scientific_advisor/ScaExamplesPage.js
@@ -12,8 +12,10 @@ export default function ScaExamplesPage() {
     const [fetchedData, setFetchedData] = useState(false);
     const [examples, setExamples] = useState([]);
     const [projectAreaData, setProjectAreaData] = useState([]);
+    const [projects, setProjects] = useState([]);
 
     const [showCreate, setShowCreate] = useState(false);
+    const [showAlter, setShowAlter] = useState(false);
 
     useEffect(() => {
         showExamples(examples);
@@ -23,6 +25,7 @@ export default function ScaExamplesPage() {
         setFetchedData(true);
         getProjectAreaData()
         getExamples();
+        getProjects()
     }
 
     function getExamples() {
@@ -33,8 +36,23 @@ export default function ScaExamplesPage() {
                 'Authorization': 'Bearer ' + authTokens.accessToken
             },
         }).then((response) => {
-            console.log(response.data);
+            //console.log(response.data);
             setExamples(response.data);
+        }).catch(result => {
+            console.log(result.data);
+        });
+    }
+
+    function getProjects() {
+        axios({
+            url: apiURL + '/scientific_advisor/projects',
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + authTokens.accessToken
+            },
+        }).then((response) => {
+            //console.log(response.data);
+            setProjects(response.data);
         }).catch(result => {
             console.log(result.data);
         });
@@ -50,6 +68,7 @@ export default function ScaExamplesPage() {
 
             var clickableDiv = document.createElement('div');
             clickableDiv.className = 'sca-example-file-clickable light size-22 dark-background';
+            clickableDiv.id = 'clickable-' + i;
 
             var iconImage = document.createElement('img');
             iconImage.className = 'sca-example-image';
@@ -77,6 +96,8 @@ export default function ScaExamplesPage() {
             }
             examplePermissions.style.position = 'relative';
             examplePermissions.style.top = '-27px';
+            examplePermissions.style.overflow = 'hidden';
+            examplePermissions.style.textOverflow = 'ellipsis';
 
             var exampleType = document.createElement('p');
             exampleType.innerText = 'Тип: ' + example.documentType;
@@ -149,26 +170,40 @@ export default function ScaExamplesPage() {
         }
     }
 
-    // TODO add project selection
+    function fillProjectData(projectArea) {
+        $('.loaded-project').remove();
+        for (var i = 0; i < projects.length; i++) {
+            if (projects[i].projectArea === projectArea) {
+                var areaOption = document.createElement('option');
+                areaOption.value = projects[i].projectName;
+                areaOption.innerText = projects[i].projectName;
+                areaOption.className = 'loaded-project';
+                document.getElementById('dropdown-create-project').appendChild(areaOption);
+            }
+        }
+    }
+
     // Создание образца
-    function createExample(file, type, area) {
+    function createExample(file, type, area, project) {
         var formData = new FormData();
         formData.append('documentFormType', type);
         formData.append('documentFormKind', 'Образец');
         formData.append('documentFormDescription', 'Описание образца');
         // Если равно, дать доступ всем студентам
-        if (area !== 'Все мои студенты') {
-            formData.append('documentFormViewRights', 'Только для проектной области');
-        }
-        else {
+        if (area === 'Все мои студенты') {
             formData.append('documentFormViewRights', 'Только мои студенты');
         }
-        formData.append('file', file);
-        // Если равно, дать доступ всем студентам
-        if (area !== 'Все мои студенты') {
-            console.log(area);
+        else {
             formData.append('projectArea', area);
+            if (project === 'Все проекты в программе проектов') {
+                formData.append('documentFormViewRights', 'Только для проектной области');
+            }
+            else {
+                formData.append('documentFormViewRights', 'Участники проекта');
+                formData.append('projectName', project);
+            }
         }
+        formData.append('file', file);
         axios({
             url: apiURL + '/scientific_advisor/document/upload',
             method: 'POST',
@@ -182,6 +217,74 @@ export default function ScaExamplesPage() {
             window.location.reload();
         }).catch(result => {
             console.log(result);
+        });
+    }
+
+    function fillAlterAreaData() {
+        var exampleId = $('.sca-example-file-clickable-selected').attr('id').split('-')[1];
+        for (var i = 0; i < projectAreaData.length; i++) {
+            var areaOption = document.createElement('option');
+            areaOption.value = projectAreaData[i];
+            areaOption.innerText = projectAreaData[i];
+            document.getElementById('dropdown-alter-area').appendChild(areaOption);
+        }
+        if (examples[exampleId].area === 'Программа проектов не назначена') {
+            document.getElementById('dropdown-alter-area').value = 'Все мои студенты';
+        }
+        else {
+            document.getElementById('dropdown-alter-area').value = examples[exampleId].area;
+            if (examples[exampleId].area !== 'Программа проектов не назначена') {
+                fillAlterProjectData(examples[exampleId].area, examples[exampleId].project);
+                document.getElementById('dropdown-alter-project').disabled = false;
+            }
+        }
+    }
+
+    function fillAlterProjectData(projectArea, project) {
+        $('.loaded-project').remove();
+        for (var i = 0; i < projects.length; i++) {
+            if (projects[i].projectArea === projectArea) {
+                var areaOption = document.createElement('option');
+                areaOption.value = projects[i].projectName;
+                areaOption.innerText = projects[i].projectName;
+                areaOption.className = 'loaded-project';
+                document.getElementById('dropdown-alter-project').appendChild(areaOption);
+            }
+        }
+        if (project != null) {
+            document.getElementById('dropdown-alter-project').value = project;
+            console.log(document.getElementById('dropdown-alter-project').value);
+        }
+    }
+
+    function alterExamplePermissions(example, area, project) {
+        var formData = new FormData();
+        formData.append('documentName', example.documentName);
+        if (area === 'Все мои студенты') {
+            formData.append('newViewRights', 'Только мои студенты');
+        }
+        else {
+            if (project !== 'Все проекты в программе проектов') {
+                formData.append('newViewRights', 'Участники проекта');
+                formData.append('projectName', project);
+            }
+            else {
+                formData.append('newViewRights', 'Только для проектной области');
+                formData.append('projectArea', area);
+            }
+        }
+        axios({
+            url: apiURL + '/scientific_advisor/document/change/view_rights/',
+            method: 'PUT',
+            data: formData,
+            headers: {
+                'Authorization': 'Bearer ' + authTokens.accessToken
+            },
+        }).then((response) => {
+            //console.log(response.data);
+            window.location.reload(true);
+        }).catch(result => {
+            console.log(result.data);
         });
     }
 
@@ -245,6 +348,18 @@ export default function ScaExamplesPage() {
             $('#example-file-input').trigger('click');
         });
 
+        $('#change-permissions-button').off().on('click', function () {
+            setShowAlter(true);
+        });
+
+        $('#alter-example-button').off().on('click', function () {
+            var exampleId = $('.sca-example-file-clickable-selected').attr('id').split('-')[1];
+            var example = examples[exampleId];
+            var area = $('#dropdown-alter-area :selected').val();
+            var project = $('#dropdown-alter-project :selected').val();
+            alterExamplePermissions(example, area, project);
+        });
+
     });
 
     return (
@@ -266,15 +381,17 @@ export default function ScaExamplesPage() {
             <Modal centered show={showCreate} onEnter={(e) => { fillAreaData(); }} onHide={(e) => { setShowCreate(false); }} className='dark'>
                 <Modal.Header className='light-background sca-examples-modal1-header' closeButton>
                     <Modal.Title className='size-30'>
-                        <p style={{ height: '50px', marginBottom: '0px', marginLeft: '200px' }}>Загрузить образец</p>
+                        <p style={{ height: '50px', marginBottom: '0px', marginLeft: '250px' }}>Загрузить образец</p>
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body className='light-background sca-examples-modal1-body'>
                     <select id='dropdown-create-type' defaultValue='' onChange={(e) => {
-                        if ($('#dropdown-create-type :selected').val() !== '' && $('#dropdown-create-area :selected').val() !== '') {
+                        if ($('#dropdown-create-type :selected').val() !== '' && $('#dropdown-create-area :selected').val() === 'Все мои студенты') {
                             document.getElementById('create-example-button').disabled = false;
                         }
-
+                        if ($('#dropdown-create-type :selected').val() !== '' && $('#dropdown-create-area :selected').val() !== '' && $('#dropdown-create-project :selected').val() !== '') {
+                            document.getElementById('create-example-button').disabled = false;
+                        }
                     }} className='dark size-24 sca-examples-dropdown'>
                         <option value='' disabled hidden>Выберите тип образца</option>
                         <option value='Научно-исследовательская работа'>Научно-исследовательская работа</option>
@@ -283,25 +400,87 @@ export default function ScaExamplesPage() {
                         <option value='ВКР'>ВКР</option>
                     </select>
                     <select id='dropdown-create-area' defaultValue='' onChange={(e) => {
-                        if ($('#dropdown-create-type :selected').val() !== '' && $('#dropdown-create-area :selected').val() !== '') {
+                        document.getElementById('dropdown-create-project').value = '';
+                        document.getElementById('create-example-button').disabled = true;
+                        if ($('#dropdown-create-area :selected').val() === 'Все мои студенты') {
+                            document.getElementById('dropdown-create-project').disabled = true;
+                        }
+                        else {
+                            document.getElementById('dropdown-create-project').disabled = false;
+                        }
+                        if ($('#dropdown-create-type :selected').val() !== '' && $('#dropdown-create-area :selected').val() === 'Все мои студенты') {
                             document.getElementById('create-example-button').disabled = false;
                         }
+                        fillProjectData($('#dropdown-create-area :selected').val());
                     }} className='dark size-24 sca-examples-dropdown'>
-                        <option value='' disabled hidden>Выберите права доступа к образцу</option>
+                        <option value='' disabled hidden>Выберите права доступа к образцу по программе проектов</option>
                         <option value='Все мои студенты'>Все мои студенты</option>
                     </select>
-                    <button id='create-example-button' disabled className='size-24 dark-background light sca-modal-button' style={{ marginLeft: '130px' }}>
+
+                    <select disabled id='dropdown-create-project' defaultValue='' onChange={(e) => {
+                        if ($('#dropdown-create-type :selected').val() !== '' && $('#dropdown-create-area :selected').val() !== '' && $('#dropdown-create-project :selected').val() !== '') {
+                            document.getElementById('create-example-button').disabled = false;
+                        }
+                        else {
+                            document.getElementById('create-example-button').disabled = true;
+                        }
+                    }} className='dark size-24 sca-examples-dropdown'>
+                        <option value='' disabled hidden>Выберите права доступа к образцу по проекту</option>
+                        <option value='Все проекты в программе проектов'>Все проекты в программе проектов</option>
+                    </select>
+
+                    <button id='create-example-button' disabled className='size-24 dark-background light sca-modal-button' style={{ marginLeft: '180px' }}>
                         Загрузить файл и создать образец
                     </button>
                     <input id='example-file-input' type='file' style={{ display: 'none' }} onChange={(e) => {
                         if (e.target.files.length !== 0) {
                             document.getElementById('create-example-button').disabled = true;
-                            createExample(e.target.files[0], $('#dropdown-create-type :selected').val(), $('#dropdown-create-area :selected').val());
+                            createExample(e.target.files[0], $('#dropdown-create-type :selected').val(), $('#dropdown-create-area :selected').val(), $('#dropdown-create-project :selected').val());
                         }
                     }} />
                 </Modal.Body>
             </Modal>
 
+            <Modal centered show={showAlter} onEnter={(e) => { fillAlterAreaData() }} onHide={(e) => { setShowAlter(false); }} className='dark'>
+                <Modal.Header className='light-background sca-examples-modal1-header' closeButton>
+                    <Modal.Title className='size-30'>
+                        <p style={{ height: '50px', marginBottom: '0px', marginLeft: '250px' }}>Изменить образец</p>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className='light-background sca-examples-modal1-body'>
+                    <select id='dropdown-alter-area' defaultValue='' onChange={(e) => {
+                        document.getElementById('dropdown-alter-project').value = '';
+                        document.getElementById('alter-example-button').disabled = true;
+                        if ($('#dropdown-alter-area :selected').val() === 'Все мои студенты') {
+                            document.getElementById('dropdown-alter-project').disabled = true;
+                            document.getElementById('alter-example-button').disabled = false;
+                        }
+                        else {
+                            document.getElementById('dropdown-alter-project').disabled = false;
+                        }
+                        fillAlterProjectData($('#dropdown-alter-area :selected').val(), null);
+                    }} className='dark size-24 sca-examples-dropdown'>
+                        <option value='' disabled hidden>Выберите права доступа к образцу по программе проектов</option>
+                        <option value='Все мои студенты'>Все мои студенты</option>
+                    </select>
+
+                    <select disabled id='dropdown-alter-project' defaultValue='' onChange={(e) => {
+                        if ($('#dropdown-alter-area :selected').val() !== '' && $('#dropdown-alter-project :selected').val() !== '') {
+                            document.getElementById('alter-example-button').disabled = false;
+                        }
+                        else {
+                            document.getElementById('alter-example-button').disabled = true;
+                        }
+                    }} className='dark size-24 sca-examples-dropdown'>
+                        <option value='' disabled hidden>Выберите права доступа к образцу по проекту</option>
+                        <option value='Все проекты в программе проектов'>Все проекты в программе проектов</option>
+                    </select>
+
+                    <button id='alter-example-button' disabled className='size-24 dark-background light sca-modal-button' style={{ marginLeft: '180px' }}>
+                        Изменить права доступа к образцу
+                    </button>
+                </Modal.Body>
+            </Modal>
         </div>
     );
 }
